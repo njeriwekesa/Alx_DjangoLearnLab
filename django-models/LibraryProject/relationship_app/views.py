729e44733e11f2cb750723cb1118a08f1190df3d
Charity Njeri <wekesacharity@gmail.com>
2026-01-17 10:25:22 +0300

    fix list_book view in relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
 # Create your views here.
 
 def list_books(request):
-  books =Book.objects.all()
+  books = Book.objects.all()
+  return render(
+    request, 'relationship_app/list_books.html', {'books': books}
+  )
   
-  output = []
-  for book in books:
-    output.append(f"{book.title} by {book.author.name}")
 
-  return HttpResponse("\n".join(output))  
 
 class LibraryDetailView(DetailView):
   model = Library
