a970c69bf9301500498b808142fcb6602250d09f
Charity Njeri <wekesacharity@gmail.com>
2026-01-18 06:41:16 +0300

    Implement role-based access control for relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Author(models.Model):
   name = models.CharField(max_length=200)
@@ -28,3 +29,23 @@ class Librarian(models.Model):
 
   def __str__(self):
     return self.name
+  
+#UserProfile
+class UserProfile(models.Model):
+  ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+  )  
+
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+  def __str_(self):
+    return f"{self.user.username} - {self.role}"
+  
+#Automatic profile creation
+@receiver(post_save, sender=User)  
+def create_user_profile(sender, instance, created, **kwargs):
+  if created:
+    UserProfile.objects.create(user=instance)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm 
 from .forms import UserRegisterForm 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
+
 
 
 #Book view 
@@ -55,3 +57,30 @@ def login_view(request):
 def logout_view(request):
   logout(request)
   return render(request, 'relationship_app/logout.html')
+
+
+
+#UserProfile role check helpers
+def is_admin(user):
+  return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+  return user.is_authenticated and user.profile.role == 'Librarian'
+
+def is_member(user):
+  return user.is_authenticated and user.profile.role == 'Member'
+
+
+
+#UserProfile role based views
+@user_passes_test(is_admin)
+def admin_view(request):
+  return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+  return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+  return render(request, 'relationship_app/member_view.html')
