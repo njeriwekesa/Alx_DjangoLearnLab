ae1e8d85fd3cb07a3f0511b98a0e94094827f283
Charity Njeri <wekesacharity@gmail.com>
2026-01-18 04:50:53 +0300

    Fix auth URL patterns to use LoginView and LogoutView with template_name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
-from .views import register_view
+from .views import list_books, LibraryDetailView, register_view
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
   path('books/', list_books, name='list_books'),
@@ -11,5 +10,5 @@ urlpatterns = [
   #Auth URLs
   path('register/', register_view, name='register'),
   path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-  path('logout/', LogoutView.as_view('relationship_app/logout.html'), name='logout'),
+  path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
