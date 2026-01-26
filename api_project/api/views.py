96b6c6fcfa999b6a1aca456e110d24cbf5ab9581
Charity Njeri <wekesacharity@gmail.com>
2026-01-26 16:12:08 +0300

    Add: CRUD operations using BookViewSet and router

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,4 +7,8 @@ from .serializers import BookSerializer
 # BookList
 class BookList(generics.ListAPIView):
   queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+  querryset = Book.objects.all()
   serializer_class = BookSerializer
