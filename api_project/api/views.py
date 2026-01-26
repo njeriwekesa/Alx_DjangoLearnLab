d5e3b554b6661f295870a0417203ad9290296b89
Charity Njeri <wekesacharity@gmail.com>
2026-01-26 12:50:34 +0300

    Fix: use generics.ListAPIView for checker compliance

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
 
 # BookList
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
