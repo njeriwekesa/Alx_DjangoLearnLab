aa70c5aa13d30ec29be5955d9163a32906cd5d9c
Charity Njeri <wekesacharity@gmail.com>
2026-02-05 18:54:44 +0300

    Add: generic CRUD views for Book with permissions

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -22,3 +22,5 @@ class Book(models.Model):
 
   def __str__(self):
     return self.title
+  
+  
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+"""
+ListView retrieves a list of books. 
+Accessible to both authenticated and unauthenticated users
+"""
+class BookListView(generics.ListAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.AllowAny]
+
+
+"""
+DetailView retrieves a single book by its ID.
+Accessible to both authenticated and unauthenticated users.
+"""
+class BookDetailView(generics.RetrieveAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.AllowAny]
+
+
+"""
+CreateView creates a new book entry.
+Restricted to authenticated users only.
+"""
+class BookCreateView(generics.CreateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+
+"""
+UpdateView updates an existing book.
+Restricted to authenticated users only.
+"""  
+class BookUpdateView(generics.UpdateAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+
+"""
+DeleteView deletes a book.
+Restricted to authenticated users only.
+"""  
+class BookDeleteView(generics.DestroyAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [permissions.IsAuthenticated]
