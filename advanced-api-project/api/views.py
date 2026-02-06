02f5fe0e678100c14269afcd617bb3c73f640bf8
Charity Njeri <wekesacharity@gmail.com>
2026-02-06 12:00:14 +0300

    feat: add filtering, search, and ordering to book list API

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 
 
